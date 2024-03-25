052aeb8cfaf6815dd58a215ccaca3865faf928e4
codingisfun <61808280+jennyq7@users.noreply.github.com>
2024-03-25 18:20:25 -0400

    set up files and folders for project

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import './HomePage.scss';
+
+function HomePage () {
+    return(
+        <section className="home">
+            <p>test test test</p>
+        </section>
+    )
+};
+
+export default HomePage;
